ae1731c20dab4ff95cb9828c55f4b21b848d19eb
Seunghwan Hong <hj332921@gmail.com>
2020-06-25 20:02:09 +0900

    도메인 변경 사항 반영
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

---
+++ precache-manifest.476f1910b332fa57a05d6dd5ac2d99e5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135650568b595965f3a88ecddc205c64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf26abfac93c96ff4e0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "51932d8565277c1e05a9",
+    "url": "/static/css/main.59ef2dc6.chunk.css"
+  },
+  {
+    "revision": "dcf26abfac93c96ff4e0",
+    "url": "/static/js/2.5a7e8dfa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5a7e8dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51932d8565277c1e05a9",
+    "url": "/static/js/main.1a08b12a.chunk.js"
+  },
+  {
+    "revision": "6563055f66d5bd928e4f",
+    "url": "/static/js/runtime-main.3ab6d52f.js"
+  },
+  {
+    "revision": "6f902e4e9642dad1c2de309de6c7fdcc",
+    "url": "/static/media/angelhack_logo.6f902e4e.png"
+  },
+  {
+    "revision": "1a42d0bf5db2499c927cced987500a6b",
+    "url": "/static/media/business_model.1a42d0bf.png"
+  },
+  {
+    "revision": "12015ef184748facbf372ad42a256c14",
+    "url": "/static/media/commonComputer.12015ef1.png"
+  },
+  {
+    "revision": "7109711f991994772d7e1b662366a0a3",
+    "url": "/static/media/completion.7109711f.png"
+  },
+  {
+    "revision": "9162b2565d5e8de4d5dea3b751f55388",
+    "url": "/static/media/creativity.9162b256.png"
+  },
+  {
+    "revision": "84ffd4f2f20f0c1dbfd0ced60539d537",
+    "url": "/static/media/dCamp.84ffd4f2.png"
+  },
+  {
+    "revision": "80a1c6b4771f17d0b436cef57b3ea2ba",
+    "url": "/static/media/down.80a1c6b4.svg"
+  },
+  {
+    "revision": "3ea7d2c7c95f3a29708028997c927314",
+    "url": "/static/media/fastCampus.3ea7d2c7.png"
+  },
+  {
+    "revision": "0ba018089c1b7f66b681d91ace0312ec",
+    "url": "/static/media/mission_business.0ba01808.png"
+  },
+  {
+    "revision": "247a7a042ac0a03f71c79aedb3f4f31e",
+    "url": "/static/media/mission_business_background.247a7a04.png"
+  },
+  {
+    "revision": "2e2998574d85e65016e3a946a5d05afa",
+    "url": "/static/media/mission_coding.2e299857.png"
+  },
+  {
+    "revision": "4acbc020eb49228cef8beb61cc3702b6",
+    "url": "/static/media/mission_coding_background.4acbc020.png"
+  },
+  {
+    "revision": "17535d2b37de4f992492fbba8af87610",
+    "url": "/static/media/mission_education.17535d2b.png"
+  },
+  {
+    "revision": "de35a7dae7580e8cbbce2f7c8ab514cf",
+    "url": "/static/media/mission_education_background.de35a7da.png"
+  },
+  {
+    "revision": "8b24cff39f93d89f27831150c3df3221",
+    "url": "/static/media/slider.8b24cff3.svg"
+  },
+  {
+    "revision": "11159a978527465757d22886fbf87cda",
+    "url": "/static/media/sns_facebook.11159a97.svg"
+  },
+  {
+    "revision": "748b2f2b54ae5c9d4a82e9081d64b4b2",
+    "url": "/static/media/sns_instagram.748b2f2b.svg"
+  },
+  {
+    "revision": "5501250a626b7b75877fb3332cfab371",
+    "url": "/static/media/sns_twitter.5501250a.svg"
+  },
+  {
+    "revision": "858eeefb75da87907fb6bbfda1eb75cc",
+    "url": "/static/media/tableManager.858eeefb.png"
+  },
+  {
+    "revision": "34a8cdcc0d646f0edf6e026a53267900",
+    "url": "/static/media/team.34a8cdcc.png"
+  },
+  {
+    "revision": "806c53ab6697cf4dfaa350c4927f4576",
+    "url": "/static/media/ui_ux.806c53ab.png"
+  },
+  {
+    "revision": "80fab677b2635914a39c9ffdab91abfb",
+    "url": "/static/media/up.80fab677.svg"
+  },
+  {
+    "revision": "bf8eab74eb25f02be44f1383f773d079",
+    "url": "/static/media/wooah.bf8eab74.png"
+  },
+  {
+    "revision": "128e366fb62525d25fc6c22c88c830ac",
+    "url": "/static/media/강경완.128e366f.png"
+  },
+  {
+    "revision": "acdea835038966e85180a13234695f67",
+    "url": "/static/media/강상진.acdea835.png"
+  },
+  {
+    "revision": "407773982d9c13817a4988705a111184",
+    "url": "/static/media/김민우.40777398.png"
+  },
+  {
+    "revision": "a7d3142b5e0a93def8c30e1b48f82c87",
+    "url": "/static/media/김영웅.a7d3142b.png"
+  },
+  {
+    "revision": "6feff045eb352c928e5acbb2ec12f219",
+    "url": "/static/media/김진아.6feff045.png"
+  },
+  {
+    "revision": "dcaf14fdb6f983c11fd96a424e0fdff7",
+    "url": "/static/media/김진우.dcaf14fd.png"
+  },
+  {
+    "revision": "086a9858012132d98f7f6b10c295afa6",
+    "url": "/static/media/박지민.086a9858.png"
+  },
+  {
+    "revision": "d66952e2cfa306bde9d9fbd274f44274",
+    "url": "/static/media/이동건.d66952e2.png"
+  },
+  {
+    "revision": "c2117b15e392c1531a65774656ad66c3",
+    "url": "/static/media/이희택.c2117b15.png"
+  },
+  {
+    "revision": "4d3dd2f3de12042d1e1c21cb8a898b9b",
+    "url": "/static/media/정성민.4d3dd2f3.png"
+  },
+  {
+    "revision": "6c7631564e871ccad19f5a0c0e1218f4",
+    "url": "/static/media/조난현.6c763156.png"
+  },
+  {
+    "revision": "c0fdc6e6058ec4fae17e2e519c8d7286",
+    "url": "/static/media/하희목.c0fdc6e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/welcome/precache-manifest.eb045fbc4a21f618c4b55f357751dbed.js"
+  "/precache-manifest.476f1910b332fa57a05d6dd5ac2d99e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/welcome/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
